cd7c2cac4e87c93ac3a7754b2d1023cb54a2b0a3
pitems <pipe.rudloff@gmail.com>
2026-08-11 17:36:42 -0400

    Fixed icon issue on bufferline
    - The problem was inside mini_icons default colors

--- lua/luna/groups/mini_icons.lua
+++ lua/luna/groups/mini_icons.lua
@@ -2,15 +2,17 @@ local M = {}
 
 function M.get(c)
   return {
-    MiniIconsGrey = { fg = c.fg },
-    MiniIconsPurple = { fg = c.type },
-    MiniIconsBlue = { fg = c.func },
-    MiniIconsAzure = { fg = c.info },
-    MiniIconsCyan = { fg = c.parameter },
-    MiniIconsGreen = { fg = c.ok },
-    MiniIconsYellow = { fg = c.warning },
-    MiniIconsOrange = { fg = c.keyword },
-    MiniIconsRed = { fg = c.error },
+    -- Explicit transparency matters in tabline segments: without it,
+    -- Neovim can render the terminal's black background behind each icon.
+    MiniIconsGrey = { bg = "NONE", fg = c.fg },
+    MiniIconsPurple = { bg = "NONE", fg = c.type },
+    MiniIconsBlue = { bg = "NONE", fg = c.func },
+    MiniIconsAzure = { bg = "NONE", fg = c.info },
+    MiniIconsCyan = { bg = "NONE", fg = c.parameter },
+    MiniIconsGreen = { bg = "NONE", fg = c.ok },
+    MiniIconsYellow = { bg = "NONE", fg = c.warning },
+    MiniIconsOrange = { bg = "NONE", fg = c.keyword },
+    MiniIconsRed = { bg = "NONE", fg = c.error },
   }
 end
 
