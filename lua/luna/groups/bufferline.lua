67b8537937de0447e9162522762c40a78dcd88fe
pitems <pipe.rudloff@gmail.com>
2026-08-17 23:36:48 -0400

    TEsting buffer fix

--- lua/luna/groups/bufferline.lua
+++ lua/luna/groups/bufferline.lua
@@ -1,41 +1,42 @@
 local M = {}
 
 function M.get(c, opts)
-  local bg = opts.transparent and "NONE" or c.bg_alt
+  local bg = opts.transparent and "NONE" or c.tabline
+  local selected_bg = opts.transparent and "NONE" or c.bg
 
   return {
     BufferLineFill = { bg = bg },
     BufferLineBackground = { bg = bg, fg = c.grey },
     BufferLineBuffer = { bg = bg, fg = c.grey_light },
     BufferLineBufferVisible = { bg = bg, fg = c.grey_light },
-    BufferLineBufferSelected = { bg = bg, fg = c.fg_bright },
+    BufferLineBufferSelected = { bg = selected_bg, fg = c.fg_bright },
     BufferLineTab = { bg = bg, fg = c.grey },
-    BufferLineTabSelected = { bg = bg, fg = c.silver },
+    BufferLineTabSelected = { bg = selected_bg, fg = c.silver },
     BufferLineSeparator = { bg = bg, fg = c.border },
     BufferLineSeparatorVisible = { bg = bg, fg = c.border },
-    BufferLineSeparatorSelected = { bg = bg, fg = c.border },
+    BufferLineSeparatorSelected = { bg = selected_bg, fg = c.border },
     BufferLineTabSeparator = { bg = bg, fg = c.border },
     BufferLineTabSeparatorVisible = { bg = bg, fg = c.border },
-    BufferLineTabSeparatorSelected = { bg = bg, fg = c.border },
-    BufferLineIndicatorSelected = { bg = bg, fg = c.signal },
+    BufferLineTabSeparatorSelected = { bg = selected_bg, fg = c.border },
+    BufferLineIndicatorSelected = { bg = selected_bg, fg = c.signal },
     BufferLineIndicatorVisible = { bg = bg, fg = c.signal },
     BufferLineModified = { bg = bg, fg = c.warning },
     BufferLineModifiedVisible = { bg = bg, fg = c.warning },
-    BufferLineModifiedSelected = { bg = bg, fg = c.warning },
+    BufferLineModifiedSelected = { bg = selected_bg, fg = c.warning },
     BufferLineCloseButton = { bg = bg, fg = c.grey },
     BufferLineCloseButtonVisible = { bg = bg, fg = c.grey },
-    BufferLineCloseButtonSelected = { bg = bg, fg = c.error },
+    BufferLineCloseButtonSelected = { bg = selected_bg, fg = c.error },
     BufferLineDuplicate = { bg = bg, fg = c.grey_light },
     BufferLineDuplicateVisible = { bg = bg, fg = c.grey_light },
-    BufferLineDuplicateSelected = { bg = bg, fg = c.fg_bright },
+    BufferLineDuplicateSelected = { bg = selected_bg, fg = c.fg_bright },
     BufferLineError = { bg = bg, fg = c.error },
-    BufferLineErrorSelected = { bg = bg, fg = c.error },
+    BufferLineErrorSelected = { bg = selected_bg, fg = c.error },
     BufferLineWarning = { bg = bg, fg = c.warning },
-    BufferLineWarningSelected = { bg = bg, fg = c.warning },
+    BufferLineWarningSelected = { bg = selected_bg, fg = c.warning },
     BufferLineInfo = { bg = bg, fg = c.info },
-    BufferLineInfoSelected = { bg = bg, fg = c.info },
+    BufferLineInfoSelected = { bg = selected_bg, fg = c.info },
     BufferLineHint = { bg = bg, fg = c.hint },
-    BufferLineHintSelected = { bg = bg, fg = c.hint },
+    BufferLineHintSelected = { bg = selected_bg, fg = c.hint },
   }
 end
 
--- lua/luna/groups/mini_icons.lua
+++ lua/luna/groups/mini_icons.lua
@@ -31,10 +31,10 @@ function M.get(c)
     Red = c.error,
   }
   for name, fg in pairs(colors) do
-    groups["BufferLineMiniIcons" .. name] = { bg = c.bg_alt, fg = fg }
-    groups["BufferLineMiniIcons" .. name .. "Inactive"] = { bg = c.bg_alt, fg = fg }
-    groups["BufferLineMiniIcons" .. name .. "Visible"] = { bg = c.bg_alt, fg = fg }
-    groups["BufferLineMiniIcons" .. name .. "Selected"] = { bg = c.bg_alt, fg = fg }
+    groups["BufferLineMiniIcons" .. name] = { bg = c.tabline, fg = fg }
+    groups["BufferLineMiniIcons" .. name .. "Inactive"] = { bg = c.tabline, fg = fg }
+    groups["BufferLineMiniIcons" .. name .. "Visible"] = { bg = c.tabline, fg = fg }
+    groups["BufferLineMiniIcons" .. name .. "Selected"] = { bg = c.bg, fg = fg }
   end
 
   return groups
